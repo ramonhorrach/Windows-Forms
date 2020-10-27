a67169922522520cb2f27db4072c615e06325a82
ramonhorrach@paucasesnovescifp.cat <ramonhorrach@paucasesnovescifp.cat>
2020-10-27 10:52:42 +0100

    Ejercicio 1

--- Ejercicio 1/Form1.cs
+++ Ejercicio 1/Form1.cs
@@ -16,5 +16,10 @@ namespace Ejercicio_1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
     }
 }

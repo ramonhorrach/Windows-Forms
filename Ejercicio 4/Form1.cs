f7e7240244b6e3466c85521bd23780b9bc46d5e6
ramonhorrach@paucasesnovescifp.cat <ramonhorrach@paucasesnovescifp.cat>
2020-10-27 11:42:58 +0100

    Ejercicio 4

--- Ejercicio 4/Form1.cs
+++ Ejercicio 4/Form1.cs
@@ -16,5 +16,39 @@ namespace Ejercicio_4
         {
             InitializeComponent();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "+";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "-";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int operando1 = Convert.ToInt32(textBox1.Text);
+            int operando2 = Convert.ToInt32(textBox2.Text);
+            int resultado;
+
+            if (textBox4.Text.Equals("+"))
+            {
+                resultado = operando1 + operando2;
+            }
+            else {
+                resultado = operando1 - operando2;
+            }
+
+            textBox3.Text = resultado.ToString();
+        }
     }
 }

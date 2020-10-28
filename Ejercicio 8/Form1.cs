854c19157b2d0cb8a9c7cc9dafabb35d2e93f485
ramonhorrach@paucasesnovescifp.cat <ramonhorrach@paucasesnovescifp.cat>
2020-10-28 16:56:51 +0100

    Ejercicio 8

--- Ejercicio 8/Form1.cs
+++ Ejercicio 8/Form1.cs
@@ -16,5 +16,40 @@ namespace Ejercicio_8
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string student1 = "Joan";
+            string student2 = "Pep";
+            string student3 = "Tomeu";
+
+            listBox1.Items.Add(student1);
+            listBox1.Items.Add(student2);
+            listBox1.Items.Add(student3);
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string student = listBox1.SelectedItem.ToString();
+
+            textBox1.Text = student;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string student4 = "Maria";
+            string student5 = "Eloi";
+            string student6 = "Ramon";
+
+            listBox1.Items.Add(student4);
+            listBox1.Items.Add(student5);
+            listBox1.Items.Add(student6);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
     }
 }
